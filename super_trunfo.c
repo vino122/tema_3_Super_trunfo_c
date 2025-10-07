545ec0b2e4f29dff775d9d8fe62b90dc76989bf3
Vinicius Felipe de Araujo <viniciusfelipe.felip@gmail.com>
2025-10-07 17:41:17 +0000

    save 07/10

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h> // biblioteca para usar o system("clear");
 
 int main(){ 
 
@@ -12,7 +13,7 @@ int main(){
     float densidade_pop1, densidade_pop2;
     float pib_percapita1, pib_percapita2;
     int compararj1, compararj2;
-    int resultado1, resultado2, resultado3, resultado4;
+    int soma1, soma2;
  
 
     //carta numero 1
@@ -39,6 +40,8 @@ int main(){
 
     printf("Pontos turísticos: \n");
     scanf("%d", &pontos_turisticos1);
+
+    system ("clear"); //limpar a tela carta 1
 
     //calculo da densidade populacional e do pib per capita
     densidade_pop1 = populacao1 / area_km1;
@@ -86,6 +89,15 @@ int main(){
     printf("4 - PIB\n");
     scanf("%d", &compararj1);
 
+    printf("Jogador 2, é a sua vez de escolher a categoria.\n");
+    
+    printf("Escolha a categoria para comparar as cartas:\n");
+    printf("1 - Pontos Turísticos\n");
+    printf("2 - População\n");
+    printf("3 - Área em KM²\n");
+    printf("4 - PIB\n");
+    scanf("%d", &compararj2);
+
     switch (compararj1){
         case 1:
             printf("Carta escolhida: Pontos Turísticos\n");
@@ -94,6 +106,7 @@ int main(){
             
         case 2:
             printf("Carta escolhida: População\n");
+            break;
 
             
         case 3:
@@ -112,61 +125,99 @@ int main(){
         printf("Empate na escolha da categoria! Escolha novamente.\n");
         
     } else{
-        printf("Jogador 2, é a sua vez de escolher a categoria.\n");
-    
-       printf("Escolha a categoria para comparar as cartas:\n");
-        printf("1 - Pontos Turísticos\n");
-        printf("2 - População\n");
-        printf("3 - Área em KM²\n");
-        printf("4 - PIB\n");
-        scanf("%d", &compararj2);
+        
 
     switch (compararj2){
         case 1:
         printf("Carta escolhida: Pontos Turísticos\n");
-        resultado1 = pontos_turisticos1 > pontos_turisticos2 ? 1 : 0; 
-
-       
+            if (pontos_turisticos1 > pontos_turisticos2)
+            {
+                printf("Jogador 1 venceu!\n");
+            } else if (pontos_turisticos1 == pontos_turisticos2){
+                printf("Empate!\n");
+            } else {
+                printf("Jogador 2 Venceu\n");
+            }
             break;
 
         case 2:
-            printf("Carta escolhida: População\n");
-            resultado2 = populacao1 > populacao2 ? 1 : 0;
-
+        printf("Carta escolhida: População\n");
+            if (populacao1 > populacao2)
+            {
+                printf("Jogador 1 venceu!\n");
+            } else if (populacao1 == populacao2){
+                printf("Empate!\n");
+            } else {
+                printf("Jogador 2 Venceu\n");
+            }
         
             break;
 
         case 3:
-            printf("Carta escolhida: Área em KM²\n");
-            resultado3 = area_km1 > area_km2 ? 1 : 0;
-
-        
+        printf("Carta escolhida: Área em KM²\n");
+            if (area_km1 > area_km2)
+            {
+                printf("Jogador 1 venceu!\n");
+            } else if (area_km1 == area_km2){
+                printf("Empate!\n");
+            } else {
+                printf("Jogador 2 Venceu\n");
+            }
             break;
-        case 4:
-            printf("Carta escolhida: PIB\n");
-            resultado4 = pib1 > pib2 ? 1 : 0;
 
+        case 4:
+        printf("Carta escolhida: PIB\n");
+            if (pib1 > pib2)
+            {
+                printf("Jogador 1 venceu!\n");
+            } else if (pib1 == pib2){
+                printf("Empate!\n");
+            } else {
+                printf("Jogador 2 Venceu\n");
+            }
        
         default:
             printf("Opção inválida!\n");}}
-        
-        if (compararj1 == compararj2){
-            printf("Empate na escolha da categoria! Escolha novamente.\n");
-            return 0;
-        }
 
-        if (resultado1 == 1){
-            printf("Jogador 1 venceu!\n");
-        } else if (resultado2 == 1){
-            printf("Jogador 1 venceu!\n");
-        } else if (resultado3 == 1){
-            printf("Jogador 1 venceu!\n");
-        } else if (resultado4 == 1){
-            printf("Jogador 1 venceu!\n");
-        } else {
-            printf("Jogador 2 venceu!\n");
-        }
+        printf("--------------------------------\n\n");
+    
+    soma1 = pontos_turisticos1 + populacao1 + area_km1 + pib1;
+    soma2 = pontos_turisticos2 + populacao2 + area_km2 + pib2;
 
+    if (soma1 > soma2){
+        printf("Jogador 1 é o grande vencedor com a soma total de %d pontos!\n", soma1);
+    } else if (soma1 == soma2){
+        printf("Empate na soma total de pontos: %d pontos!\n", soma1);
+    } else {
+        printf("Jogador 2 é o grande vencedor com a soma total de %d pontos!\n", soma2);
+    }
+
+    printf("--------------------------------\n\n");
+    //mostrar os detalhes das cartas 
+    printf("Detalhes da carta 1:\n");
+    printf("Letra do estado: %c\n", letra_estado1);
+    printf("Código da carta: %s\n", cod_carta1);
+    printf("Nome da cidade: %s\n", nome_cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área em KM²: %.2f\n", area_km1);
+    printf("PIB: %.2f\n", pib1);
+    printf("Pontos turísticos: %d\n", pontos_turisticos1);
+    printf("Densidade populacional: %.2f\n", densidade_pop1);
+    printf("PIB per capita: %.2f\n", pib_percapita1);
+
+    printf("--------------------------------\n\n");
+
+    printf("Detalhes da carta 2:\n");
+    printf("Letra do estado: %c\n", letra_estado2);
+    printf("Código da carta: %s\n", cod_carta2);
+    printf("Nome da cidade: %s\n", nome_cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área em KM²: %.2f\n", area_km2);
+    printf("PIB: %.2f\n", pib2);
+    printf("Pontos turísticos: %d\n", pontos_turisticos2);
+    printf("Densidade populacional: %.2f\n", densidade_pop2);
+    printf("PIB per capita: %.2f\n", pib_percapita2);
+     
 
 
 
